9cc16383c444ed5e6c4dcea69f652007831eeab6
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2025-03-03 16:46:54 +0800

    feat(携程): 全屏广告-首页弹窗广告、全屏广告-机票页面弹窗广告

--- src/apps/ctrip.android.view.ts
+++ src/apps/ctrip.android.view.ts
@@ -38,8 +38,8 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '全屏广告-弹窗广告',
-      desc: '点击关闭',
+      name: '全屏广告-首页弹窗广告',
+      desc: '点击关闭按钮',
       rules: [
         {
           fastQuery: true,
@@ -49,9 +49,26 @@ export default defineGkdApp({
           activityIds:
             'ctrip.android.publicproduct.home.view.CtripHomeActivity',
           matches:
-            '@ImageView[clickable=true] < RelativeLayout + RelativeLayout >5 [desc^="广告"] + [text="立即领取"]',
+            '@ImageView[clickable=true] < RelativeLayout + RelativeLayout >5 [desc^="广告"]',
           exampleUrls: 'https://e.gkd.li/00628604-fab9-432c-a70b-343c8c889c23',
           snapshotUrls: 'https://i.gkd.li/i/16357287',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '全屏广告-机票页面弹窗广告',
+      desc: '点击关闭按钮',
+      rules: [
+        {
+          fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          activityIds: 'ctrip.business.planthome.CtripPlantHomeActivity',
+          matches:
+            '@ImageView[clickable=true][vid="a"][width<120][height<120] <<n FrameLayout[id="android:id/content"]',
+          exampleUrls: 'https://e.gkd.li/6170a942-3fc9-42d8-9ecd-c48b2f453c98',
+          snapshotUrls: 'https://i.gkd.li/i/19050401',
         },
       ],
     },
